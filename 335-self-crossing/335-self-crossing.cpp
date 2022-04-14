83644aed6b5cd8fa4458920a9180d092cd499603
Rishabh Garg <39647733+rishabhgargdps@users.noreply.github.com>
2022-04-15 01:06:56 +0530

    Time: 26 ms (64.96%), Space: 18.8 MB (43.80%) - LeetHub

---
+++ 335-self-crossing/335-self-crossing.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSelfCrossing(vector<int>& x) {
+        int n = x.size();
+        for(int i=3; i<n; i++) {
+            if(x[i] >= x[i-2] && x[i-1] <= x[i-3]) return true;
+            if(i >= 4) {
+                if(x[i-1] == x[i-3] && x[i-2] <= x[i] + x[i-4]) return true;
+            }
+            if(i >= 5) {
+                if(x[i] + x[i-4] >= x[i-2] && x[i-1] + x[i-5] >= x[i-3] && x[i-2] > x[i-4] && x[i-3] > x[i-1]) return true;
+            }
+        }
+        return false;
+    }
+};
