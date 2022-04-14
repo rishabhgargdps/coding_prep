f894940b61e50816e22f0c35d354b83390f6d8e4
Rishabh Garg <39647733+rishabhgargdps@users.noreply.github.com>
2022-04-14 22:59:41 +0530

    Time: 664 ms (17.50%), Space: 293 MB (5.02%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long binary_search(vector<int>nums, long long left, long long right, int target) {
+        //Base case
+        if(left >= right) return left;
+        //Recursive step
+        long long mid = left + (right-left)/2;
+        long long ans = 0ll;
+        for(int i=0; i < nums.size(); i++) {
+            ans += mid / nums[i];
+        }
+        if(ans < target) return binary_search(nums, mid+1, right, target);
+        return binary_search(nums, left, mid, target);
+    }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        return binary_search(time, 1ll, LLONG_MAX/time.size(), totalTrips);
+    }
+};
