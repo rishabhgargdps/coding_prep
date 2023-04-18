70f3cf79691644a9316aadc09c6b5820a59a5e52
Rishabh Garg <39647733+rishabhgargdps@users.noreply.github.com>
2023-04-18 22:07:42 +0530

    Time: 2617 ms (5.38%), Space: 457.5 MB (10.45%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     int mod = 1e9+7;
-    int solve(int n, int total, int absent, int cons_late, vector<vector<vector<int>>>& dp) {
-        if(absent >= 2 || cons_late >= 3) return 0;
-        if(total == n) return 1;
-        if(dp[total][absent][cons_late] != -1) return dp[total][absent][cons_late];
-        dp[total][absent][cons_late] = ((long long)solve(n, total+1, absent, 0, dp)%mod + (long long)solve(n, total+1, absent+1, 0, dp)%mod + (long long)solve(n, total+1, absent, cons_late+1, dp)%mod)%mod;
-        return dp[total][absent][cons_late]%mod;
-    }
     int checkRecord(int n) {
-        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return solve(n, 0, 0, 0, dp);
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(3, vector<int>(4)));
+        dp[0][0][0] = 1;
+        for(int total=1; total<=n; total++) {
+            //if present
+            for(int absent=0; absent<2; absent++) {
+                for(int cons_late=0; cons_late<3; cons_late++) {
+                    dp[total][absent][0] = (dp[total][absent][0] + dp[total-1][absent][cons_late])%mod;
+                }
+            }
+            //if absent
+            for(int cons_late=0; cons_late<3; cons_late++) {
+                dp[total][1][0] = (dp[total][1][0] + dp[total-1][0][cons_late])%mod;
+            }
+            //if late
+            for(int absent=0; absent<2; absent++) {
+                for(int cons_late=1; cons_late<3; cons_late++) {
+                    dp[total][absent][cons_late] = (dp[total][absent][cons_late] + dp[total-1][absent][cons_late-1])%mod;
+                }
+            }
+        }
+        int res = 0;
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<3; j++) {
+                res = (res + dp[n][i][j])%mod;
+            }
+        }
+        return res;
     }
 };
