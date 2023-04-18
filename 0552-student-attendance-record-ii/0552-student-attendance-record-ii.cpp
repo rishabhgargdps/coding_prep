30361659e2ed807de675eae36b0ee8e9cc451912
Rishabh Garg <39647733+rishabhgargdps@users.noreply.github.com>
2023-04-18 22:15:23 +0530

    Time: 158 ms (82.69%), Space: 5.9 MB (95.82%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -2,30 +2,36 @@ class Solution {
 public:
     int mod = 1e9+7;
     int checkRecord(int n) {
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(3, vector<int>(4)));
-        dp[0][0][0] = 1;
+        int dp[2][3] = {0};
+        dp[0][0] = 1;
         for(int total=1; total<=n; total++) {
+            int temp_dp[2][3] = {0};
             //if present
             for(int absent=0; absent<2; absent++) {
                 for(int cons_late=0; cons_late<3; cons_late++) {
-                    dp[total][absent][0] = (dp[total][absent][0] + dp[total-1][absent][cons_late])%mod;
+                    temp_dp[absent][0] = (temp_dp[absent][0] + dp[absent][cons_late])%mod;
                 }
             }
             //if absent
             for(int cons_late=0; cons_late<3; cons_late++) {
-                dp[total][1][0] = (dp[total][1][0] + dp[total-1][0][cons_late])%mod;
+                temp_dp[1][0] = (temp_dp[1][0] + dp[0][cons_late])%mod;
             }
             //if late
             for(int absent=0; absent<2; absent++) {
                 for(int cons_late=1; cons_late<3; cons_late++) {
-                    dp[total][absent][cons_late] = (dp[total][absent][cons_late] + dp[total-1][absent][cons_late-1])%mod;
+                    temp_dp[absent][cons_late] = (temp_dp[absent][cons_late] + dp[absent][cons_late-1])%mod;
+                }
+            }
+            for(int i=0; i<2; i++) {
+                for(int j=0; j<3; j++) {
+                    dp[i][j] = temp_dp[i][j];
                 }
             }
         }
         int res = 0;
         for(int i=0; i<2; i++) {
             for(int j=0; j<3; j++) {
-                res = (res + dp[n][i][j])%mod;
+                res = (res + dp[i][j])%mod;
             }
         }
         return res;
